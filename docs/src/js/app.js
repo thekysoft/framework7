a1f1a752df1ef994895775f97404730b6fa2e9a5
thekysoft <85360081+thekysoft@users.noreply.github.com>
2025-07-11 23:38:04 +0800

    upload

---
+++ docs/src/js/app.js
@@ -0,0 +1,39 @@
+import $ from 'dom7';
+import Framework7 from 'framework7/bundle';
+
+// Import F7 Styles
+import 'framework7/css/bundle';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.css';
+
+
+// Import Routes
+import routes from './routes.js';
+// Import Store
+import store from './store.js';
+
+// Import main app component
+import App from '../app.f7';
+
+
+var app = new Framework7({
+  name: 'My App', // App name
+  theme: 'ios', // Automatic theme detection
+  colors: {
+    primary: '#ef5350',
+  },
+
+  el: '#app', // App root element
+  component: App, // App main component
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+
+  // Register service worker (only on production build)
+  serviceWorker: process.env.NODE_ENV ==='production' ? {
+    path: '/service-worker.js',
+  } : {},
+});
